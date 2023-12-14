c17c4dab5eae2a120d963c672f1420fb31c09b50
G9000 <leo.caesar@live.com>
2023-12-15 06:00:10 +0800

    add run assistant

---
+++ apps/web/app/api/assistant/run/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import { runAssistant } from "assistant/assistant";
+
+export async function POST(request: Request) {
+  try {
+    const formData = await request.formData();
+    let threadId = formData.get("threadId") as string;
+    let assistantId = formData.get("assistantId") as string;
+    let instructions = formData.get("instructions") as string;
+
+    if (!assistantId || !threadId || !instructions) {
+      return NextResponse.json(
+        { message: "fields are required" },
+        { status: 400 }
+      );
+    }
+
+    let assistant = await runAssistant({
+      assistantId,
+      threadId,
+      instructions,
+    });
+
+    return NextResponse.json(assistant);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}

318cd0701e55e8cc5a7769ae1c54a209f2b8ae02
G9000 <leo.caesar@live.com>
2023-12-15 16:43:21 +0800

    fix route

--- apps/web/app/auth/callback/route.ts
+++ apps/web/app/auth/callback/route.ts
@@ -3,8 +3,9 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const cookieStore = cookies();
@@ -12,7 +13,7 @@ export async function GET(request: Request) {
 
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect("/");
+      return NextResponse.redirect(`${origin}${next}`);
     }
   }
 
--- apps/web/app/auth/logout/rroute.ts
+++ apps/web/app/auth/logout/rroute.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request) {
 
   await supabase.auth.signOut();
 
-  return NextResponse.redirect(`${requestUrl.origin}/login`, {
+  return NextResponse.redirect(requestUrl.origin, {
     status: 301,
   });
 }

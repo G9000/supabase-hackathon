41337cd331094e41ee2be780dbd7e1985f12cdde
Bintang Yoga Pamungkas <bintang.parmo@gmail.com>
2023-12-15 10:48:18 +0700

    add composing page

--- apps/web/app/onboarding/last-step/page.tsx
+++ apps/web/app/onboarding/last-step/page.tsx
@@ -29,9 +29,14 @@ export const stageConfig2 = {
   totalStage: 2,
 };
 
+const composingRecipeConfig = {
+  label: "Composing recipe",
+  showTitle: false
+};
 
 export default function Page() {
   const [step, setStep] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleBack = () => {
     setStep((prevStep) => prevStep - 1);
@@ -45,97 +50,110 @@ export default function Page() {
     // todo
   };
 
-  return (
-    <OnboardingLayout {...config}>
-      {step >= 0 && (
-        <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-10 w-full">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.98, y: 200 }}
-            animate={{
-              opacity: 1,
-              scale: step > 0 ? 0.9 : 1,
-              y: step > 0 ? -40 : 0,
-            }}
-            transition={{
-              duration: 0.3,
-              ease: [0, 0.71, 0.2, 1.01],
-              scale: {
-                type: "spring",
-                damping: 10,
-                stiffness: 100,
-                restDelta: 0.001,
-              },
-            }}
-          >
-            <OnboardignCard
-              {...stageConfig1}
-              handleNext={handleNext}
-            >
-              <CardContent>
-                <RadioGroup defaultValue="option-one">
-                  <div className="flex items-center space-x-3">
-                    <RadioGroupItem value="option-one" id="option-one" className="w-6 h-6" />
-                    <Label htmlFor="option-one" className="font-extrabold text-2xl">A day</Label>
-                  </div>
-                  <div className="flex items-center space-x-3">
-                    <RadioGroupItem value="option-two" id="option-two" className="w-6 h-6" />
-                    <Label htmlFor="option-two" className="font-extrabold text-2xl">A Week</Label>
-                  </div>
-                  <div className="flex items-center space-x-3">
-                    <RadioGroupItem value="option-three" id="option-three" className="w-6 h-6" />
-                    <Label htmlFor="option-three" className="font-extrabold text-2xl">2 Weeks</Label>
-                  </div>
-                </RadioGroup>
+  const ComposingRecipe = () => {
+    return (
+      <OnboardingLayout {...composingRecipeConfig}>
+        <h1 className="loader mt-4 max-w-md text-center text-2xl font-extrabold opacity-50 bg-gradient-to-r from-[#0808081f] from-20% via-[#080808a2] via-50% to-[#0808081f] to-80% text-transparent bg-clip-text">I would like to have meal plan for 2 weeks with 1000 SGD. I like shoyu ramen, sashimi, okonomiyaki. I don’t like Pork, Spicy, and Salmon. My restriction is haram food, peanuts, and lactose intolerant. My fav cuisine are Chinese and Japanese food</h1>
+      </OnboardingLayout>
+    )
+  }
 
-              </CardContent>
-            </OnboardignCard>
-          </motion.div>
-        </div>
+  return (
+    <>
+      {isLoading && ComposingRecipe()}
+      {!isLoading && (
+        <OnboardingLayout {...config}>
+          {step >= 0 && (
+            <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-10 w-full">
+              <motion.div
+                initial={{ opacity: 0, scale: 0.98, y: 200 }}
+                animate={{
+                  opacity: 1,
+                  scale: step > 0 ? 0.9 : 1,
+                  y: step > 0 ? -40 : 0,
+                }}
+                transition={{
+                  duration: 0.3,
+                  ease: [0, 0.71, 0.2, 1.01],
+                  scale: {
+                    type: "spring",
+                    damping: 10,
+                    stiffness: 100,
+                    restDelta: 0.001,
+                  },
+                }}
+              >
+                <OnboardignCard
+                  {...stageConfig1}
+                  handleNext={handleNext}
+                >
+                  <CardContent>
+                    <RadioGroup defaultValue="option-one">
+                      <div className="flex items-center space-x-3">
+                        <RadioGroupItem value="option-one" id="option-one" className="w-6 h-6" />
+                        <Label htmlFor="option-one" className="font-extrabold text-2xl">A day</Label>
+                      </div>
+                      <div className="flex items-center space-x-3">
+                        <RadioGroupItem value="option-two" id="option-two" className="w-6 h-6" />
+                        <Label htmlFor="option-two" className="font-extrabold text-2xl">A Week</Label>
+                      </div>
+                      <div className="flex items-center space-x-3">
+                        <RadioGroupItem value="option-three" id="option-three" className="w-6 h-6" />
+                        <Label htmlFor="option-three" className="font-extrabold text-2xl">2 Weeks</Label>
+                      </div>
+                    </RadioGroup>
+
+                  </CardContent>
+                </OnboardignCard>
+              </motion.div>
+            </div>
+          )}
+          {step >= 1 && (
+            <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-20 w-full">
+              <motion.div
+                initial={{ opacity: 0, scale: 0.98, y: 200 }}
+                animate={{
+                  opacity: 1,
+                  scale: step > 1 ? 0.9 : 1,
+                  y: step > 1 ? -40 : 0,
+                }}
+                transition={{
+                  duration: 0.3,
+                  ease: [0, 0.71, 0.2, 1.01],
+                  scale: {
+                    type: "spring",
+                    damping: 10,
+                    stiffness: 100,
+                    restDelta: 0.001,
+                  },
+                }}
+              >
+                <OnboardignCard
+                  {...stageConfig2}
+                  handleNext={handleSubmit}
+                  handleBack={handleBack}
+                  lastStep
+                  lastStepButton={"Finish & compose"}
+                >
+                  <CardContent>
+                    <Textarea
+                      placeholder="Ex, 1000 SGD"
+                      className="border-0 pl-0 text-2xl leading-6 font-bold h-[100px] focus-visible:ring-0"
+                    />
+                    <div className="flex items-center">
+                      <Image src={'/info.svg'} width={16} height={16} alt='info' />
+                      <span className="ml-1 text-foreground/50 text-base font-extrabold">
+                        Please include your currency
+                      </span>
+                    </div>
+                  </CardContent>
+                </OnboardignCard>
+              </motion.div>
+            </div>
+          )
+          }
+        </OnboardingLayout >
       )}
-      {step >= 1 && (
-        <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-20 w-full">
-          <motion.div
-            initial={{ opacity: 0, scale: 0.98, y: 200 }}
-            animate={{
-              opacity: 1,
-              scale: step > 1 ? 0.9 : 1,
-              y: step > 1 ? -40 : 0,
-            }}
-            transition={{
-              duration: 0.3,
-              ease: [0, 0.71, 0.2, 1.01],
-              scale: {
-                type: "spring",
-                damping: 10,
-                stiffness: 100,
-                restDelta: 0.001,
-              },
-            }}
-          >
-            <OnboardignCard
-              {...stageConfig2}
-              handleNext={handleSubmit}
-              handleBack={handleBack}
-              lastStep
-              lastStepButton={"Finish & compose"}
-            >
-              <CardContent>
-                <Textarea
-                  placeholder="Ex, 1000 SGD"
-                  className="border-0 pl-0 text-2xl leading-6 font-bold h-[100px] focus-visible:ring-0"
-                />
-                <div className="flex items-center">
-                  <Image src={'/info.svg'} width={16} height={16} alt='info' />
-                  <span className="ml-1 text-foreground/50 text-base font-extrabold">
-                    Please include your currency
-                  </span>
-                </div>
-              </CardContent>
-            </OnboardignCard>
-          </motion.div>
-        </div>
-      )
-      }
-    </OnboardingLayout >
+    </>
   );
 }
--- apps/web/components/Onboarding/OnboardingLayout.tsx
+++ apps/web/components/Onboarding/OnboardingLayout.tsx
@@ -6,11 +6,13 @@ interface OnboardingLayoutPropsI {
   label?: string;
   title?: string;
   children?: ReactNode;
+  showTitle?: boolean;
 }
 
 export default function OnboardingLayout({
   label = "Label Here",
   title = "Title Here",
+  showTitle = true,
   children,
 }: OnboardingLayoutPropsI) {
   return (
@@ -19,7 +21,9 @@ export default function OnboardingLayout({
       <div className="h-full flex flex-col justify-center">
         <div className="flex flex-col items-center">
           <div className="inline-block px-4 py-3 border rounded-full text-foreground/50 font-bold border-foreground/10 text-sm">{label}</div>
-          <div className="text-2xl font-extrabold leading-6 max-w-xs mt-4 text-center bg-gradient-to-r from-[#080808CC] to-[#0808088F] text-transparent bg-clip-text">{title}</div>
+          {showTitle && (
+            <div className="text-2xl font-extrabold leading-6 max-w-xs mt-4 text-center bg-gradient-to-r from-[#080808CC] to-[#0808088F] text-transparent bg-clip-text">{title}</div>
+          )}
         </div>
         <div className="relative flex-shrink-0 w-full h-[440px]">
           {children}

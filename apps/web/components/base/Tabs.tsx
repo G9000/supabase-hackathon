a684a3295247b42d78fd77833fec9a8ef1241074
Bintang Yoga Pamungkas <bintang.parmo@gmail.com>
2023-12-17 13:36:34 +0700

    add review groceries page

--- apps/web/components/base/Tabs.tsx
+++ apps/web/components/base/Tabs.tsx
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            "inline-flex items-center justify-center border whitespace-nowrap rounded-md px-3 py-1 font-bold rounded-full text-base ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border data-[state=active]:border-foreground/60 data-[state=active]:text-foreground/80 w-10 h-10",
+            "inline-flex items-center justify-center border whitespace-nowrap px-3 py-1 font-bold rounded-full text-base ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border data-[state=active]:border-foreground/60 data-[state=active]:text-foreground/80 w-10 h-10",
             className
         )}
         {...props}
